3bf0727ae6b4e697d45a0af3d4f07a2d668baf22
Ospinajuanp <juanpo996@gmail.com>
2023-01-25 10:20:49 -0500

    Inicializando proyecto-paquete npm

---
+++ bin/global.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node 
+let query = require('../src/index.js'); // Variable con información del ejecutable
+query.greetings(); // Ejecutamos la función que queremos mostrar por la terminal
---
+++ src/index.js
@@ -0,0 +1,3 @@
+function greetings (){
+    console.log('Thank you for using the ospinajuanp-single-page package to initialize your project.');
+}
